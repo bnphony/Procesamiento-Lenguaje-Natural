beb787c3042a9cbeb6fe32a50c50fa68c11f47fd
Bryan <bryantb479@gmail.com>
2022-02-03 09:52:57 -0500

    03_02_2022_(1)

--- prueba_2/core/erp/conexiones.py
+++ prueba_2/core/erp/conexiones.py
@@ -1,23 +1,18 @@
 
 
 def crearDependencia(oraciones):
-    print("INgeso a la funcion")
+
     datos = []
     links = []
 
     conexiones = [oracion for oracion in oraciones if oracion.grupo != 0]
 
-    print(type(conexiones))
+
     for oracion in conexiones:
         preview = oracion
-        print('ingeso a l for')
-        print(conexiones[:conexiones.index(preview)] + conexiones[conexiones.index(preview) + 1:])
         for n in (conexiones[:conexiones.index(preview)] + conexiones[conexiones.index(preview) + 1:]):
-            # valor = list(set(preview + n))
             links.append([preview.id, n.id])
 
-
-    print('Paso los links')
     for link in links:
         preview = link
         for n in links[:links.index(preview)] + links[links.index(preview) + 1:]:
@@ -28,7 +23,6 @@ def crearDependencia(oraciones):
                 links[links.index(preview)] = valor
                 preview = valor
 
-    print(links)
     conexiones = []
     for link in links:
         conexiones.append({'source': link[0], 'target': link[1]})
